35087cfb2f5c37d9c6626c38d7f525d72ae912b2
Panagiotis Papadopoulos <panpap@gmx.com>
2015-12-17 18:10:51 +0100

    sadd

--- core.rb
+++ core.rb
@@ -9,7 +9,6 @@ class Core
 
 	def initialize 
 	puts "> Creating Directories..."
-	Dir.mkdir @@rootDir unless File.exists?(@@rootDir)	
         Dir.mkdir @@dataDir unless File.exists?(@@dataDir)
         Dir.mkdir @@adsDir unless File.exists?(@@adsDir)
 		Dir.mkdir @@userDir unless File.exists?(@@userDir)
