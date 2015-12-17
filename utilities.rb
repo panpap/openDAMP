f907e5bf3febec02b001f26d07abc0dacaf3e427
Panagiotis Papadopoulos <panpap@gmx.com>
2015-12-17 14:46:57 +0100

    18

--- utilities.rb
+++ utilities.rb
@@ -14,7 +14,7 @@ class Utilities
         end
 
 	def makeStats (arr)
-		result=['sum'=>0,'avg'=>0,'median'=>0,'max'=>0,'min'=>0]
+		result={'sum'=>0,'avg'=>0,'median'=>0,'max'=>0,'min'=>0}
 		if arr!=nil
 			result['sum']=arr.inject{ |s, el| s + el}.to_f
 			result['avg']=result['sum']/arr.size
