50347bdff80dfc05a759c1b69a7595bdd23d4feb
Panagiotis Papadopoulos <panpap@gmx.com>
2015-12-22 12:49:03 +0100

    m

--- ops.rb
+++ ops.rb
@@ -22,6 +22,7 @@ class Operations
 		atts.each{|a| f[a]=File.new(@defines.dirs['dataDir']+a,'w')}
         for row in @@loadedRows do
             atts.each{|att| f[att].puts row[att]}
+			separateTimelineEvents(row['tmstp'],row['url'],row['IPport'])
    		end
 		atts.each{|a| Utilities.countInstances(@defines.dirs['dataDir']+a); f[a].close}
 	end
