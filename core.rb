b969a8319de5e64adc97c29080126cf2dbafc545
Panagiotis Papadopoulos <panpap@gmx.com>
2016-01-12 16:00:23 +0100

    m

--- core.rb
+++ core.rb
@@ -77,6 +77,7 @@ class Core
 		timeline_path=@cwd+@defines.userDir+@defines.tmln_path
 		for tmln in tmlnFiles do
 			if not tmln.eql? '.' and not tmln.eql? ".." and not File.directory?(user_path+tmln)
+				puts "USER "+tmln
 				fr=File.new(user_path+tmln,'r')
 				fw=File.new(timeline_path+tmln+"_per"+@window.to_s+"msec",'w')
 				firstTime=-1
@@ -97,7 +98,7 @@ class Core
 					parseRequest(r,false)
 				end
 				fr.close;fw.close
-			break
+	break
 			end
 		end
 		bucketResults(@trace,fw)
