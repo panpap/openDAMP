9d03836b37baf714695eaff7e02acc9e679244a8
Panagiotis Papadopoulos <panpap@gmx.com>
2016-01-12 15:19:31 +0100

    m

--- core.rb
+++ core.rb
@@ -142,7 +142,7 @@ class Core
 		diff=tmstp.to_i-firstTime
 		wnum=diff.to_f/@window.to_i
 		fw.puts "WINDOW "+wnum.to_i.to_s+" "+tmstp+" "+url
-puts firstTime.to_s+" "+tmstp.to_i.to_s+" "+diff.to_s+" "+wnum.to_i
+puts firstTime.to_s+" "+tmstp.to_i.to_s+" "+diff.to_s+" "+wnum.to_i.to_s
 		return wnum.to_i
 	end
 
