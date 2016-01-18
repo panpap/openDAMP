f23d6181d912d1cde81e7a263ec8c56f39b68b22
Panagiotis Papadopoulos <panpap@gmx.com>
2016-01-18 17:55:53 +0100

    m

--- ops.rb
+++ ops.rb
@@ -90,7 +90,7 @@ class Operations
 	end
 
 	def plot(path)
-		puts "> Plotting existing output from <"+path+"> of "+@defines.traceFile+"..."
+		puts "> Plotting existing output from <"+path+">..."
 		#f=File.new(@defines.files['userFile'],'r')
 		folder=path+@defines.adsDir
 		files=Dir.entries(folder) rescue entries=Array.new
