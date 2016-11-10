1c073e7dd41156fb124a41a099d3890b0fef9329
panpap <panpap@gmx.com>
2016-11-10 18:45:24 +0100

    =

--- beaconDetectorThreads.rb
+++ beaconDetectorThreads.rb
@@ -12,7 +12,7 @@ def is_1pixel_image?(url)
 #	else	# no... wget it
 		begin
 			url="http://"+url if not url.include? "://"
-			pixels=FastImage.size(url, :timeout=>5)
+			pixels=FastImage.size(url, :timeout=>2)
 		    return pixels.to_s if pixels!=nil
 		rescue Exception => e  
 			if not e.message.include? "Network is unreachable"
@@ -24,31 +24,38 @@ def is_1pixel_image?(url)
     return nil
 end
 
-def store(line)
-	fw=File.new("beaconDetectorTHREADS.csv","a")
-	fw.puts ARGV[0]+"\t"+line.to_s
+def store(line,filename)
+	fw=File.new(filename+"beaconDetectorTHREADS.csv","a")
+	fw.puts filename+"\t"+line.to_s
 	fw.close
 end
 
-system("rm -f beaconDetectorTHREADS.csv")
 #@db = Database.new(@defines,"beaconsDB.db")
-f=File.new(ARGV[0])
-images=ARGV[0]+"THREADS_IMGsizes"
+filename=ARGV[0]
+system("rm -f "+filename+"beaconDetectorTHREADS.csv")
+f=File.new(filename)
+images=filename+"THREADS_IMGsizes"
 fw=File.new(images,"w")
 total=0
+totalLines= `wc -l "#{filename}"`.strip.split(' ')[0]
 @count=0
 start=Time.now
 threads=Array.new
 while line=f.gets
-	threads.push(Thread.new{ 
-		#q.push(line)
-			pixels=is_1pixel_image?(line)
-			Thread.current[:output]=pixels,line if pixels!=nil
-			Thread.exit})
-	if total%1000==0
-		puts "\t"+total.to_s+" lines so far... "+(Time.now - start).to_s+" seconds"
-		start=Time.now
-		sleep(7)
+	begin
+		threads.push(Thread.new{ 
+			#q.push(line)
+				pixels=is_1pixel_image?(line)
+				Thread.current[:output]=pixels,line if pixels!=nil
+				Thread.exit})
+		if total%1000==0
+			puts "\t"+total.to_s+"/"+totalLines+" lines so far... "+(Time.now - start).to_s+" seconds"
+			start=Time.now
+			sleep(10)
+		end
+	rescue ThreadError => e
+		puts "ThreadError "+e+"\n"+total.to_s+" lines"+"\nI'll sleep a little"
+		sleep(20)
 	end
 	total+=1
 end
@@ -65,5 +72,5 @@ end
 images= `wc -l "#{images}"`.strip.split(' ')[0].to_i
 f.close
 fw.close
-puts "THREADS, I found "+@count.to_s+"/"+images.to_s+" Beacons out of "+total.to_s+" examined URLs for "+ARGV[0].split("BEACONS_")[1]
-store(@count)
+puts "THREADS, I found "+@count.to_s+"/"+images.to_s+" Beacons out of "+total.to_s+" examined URLs for "+filename.split("BEACONS_")[1]
+store(@count,filename)
