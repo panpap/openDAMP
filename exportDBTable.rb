1f64209c739b8e72f3f3be51a10130bc4ed57783
panpap <panpap@gmx.com>
2016-12-15 19:02:55 +0100

    =

--- exportDBTable.rb
+++ exportDBTable.rb
@@ -56,26 +56,44 @@ def arrayCase(tbl)
 	end
 end
 
-table="userResults"
-CSV.open(table+"_summary.csv","w",{:col_sep => "\t"}) do |summary|
-	for i in 1..12
-		month="0"
-		if i.to_s.size==1
-			month="0"+i.to_s
-		else
-			month=i.to_s
-		end		
-		for j in [10,20,30]
-			dbname="results/"+i.to_s+"/2015"+month+"_"+j.to_s+"/results_month_2015"+month+"_"+j.to_s+"days_filtered_ES_sorted_uniq/month_2015"+month+"_"+j.to_s+"days_filtered_ES_sorted_uniq_analysis.db"
-			puts dbname
-			
-			db=SQLite3::Database.open dbname
-			results=getAll(db,table,nil,nil,nil,false)
-			columnNames=getColumnNames(db,table)
-			CSV.open(dbname.rpartition("/")[0]+"/"+table+".csv","w",{:col_sep => "\t"}) do |csv|
-				csv << columnNames
-				results.each { |row| csv << row; row.unshift(dbname.rpartition("/")[0].split("month_")[1].split("days")[0]); summary << row}
+def readDB(table)
+	CSV.open(table+"_summary.csv","w",{:col_sep => "\t"}) do |summary|
+		for i in 1..12
+			month="0"
+			if i.to_s.size==1
+				month="0"+i.to_s
+			else
+				month=i.to_s
+			end		
+			for j in [10,20,30]
+				dbname="results/"+"chechpoint1"+"/2015"+month+"_"+j.to_s+"/results_month_2015"+month+"_"+j.to_s+"days_filtered_ES_sorted_uniq/month_2015"+month+"_"+j.to_s+"days_filtered_ES_sorted_uniq_analysis.db"
+				if File.exist? dbname
+					db=SQLite3::Database.open dbname
+					results=getAll(db,table,nil,nil,nil,false)
+					columnNames=getColumnNames(db,table)
+					CSV.open(dbname.rpartition("/")[0]+"/"+table+".csv","w",{:col_sep => "\t"}) do |csv|
+						csv << columnNames
+						summary << columnNames
+						results.each { |row| csv << row; row.unshift(dbname.rpartition("/")[0].split("month_")[1].split("days")[0]); summary << row}
+					end
+				else
+					puts "NOTEXISTS: "+dbname
+				end
 			end
 		end
 	end
 end
+
+table="userResults"
+puts table+" -------------"
+readDB(table)
+readDB(table+"_app")
+readDB(table+"_web")
+table="traceResults"
+puts table+" -------------"
+readDB(table)
+readDB(table+"_app")
+readDB(table+"_web")
+table="csyncResults"
+puts table+" -------------"
+readDB(table)
